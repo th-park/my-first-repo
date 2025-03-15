8b1ff7855383dbc9b2c881963ea60597ad457f15
th-park <thmk.park@gmail.com>
2025-03-15 23:07:37 +0900

    Enhance stock analysis script to include additional tickers and improve trend detection logic for moving averages. Added checks for stocks below all moving averages with increased volume.

--- Python_Project/findStock.py
+++ Python_Project/findStock.py
@@ -2,16 +2,18 @@ import yfinance as yf
 import pandas as pd
 
 # 티커 목록 설정
-tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META",
+tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META", #M7
            "PYPL", "ADBE", 
            "INTC", "CSCO", "NFLX", "CMCSA", "PEP", "COST", "TMUS", "AVGO", 
            "TXN", "QCOM", "AMAT",
            "ASML", "TSM", "ARM", "SMCI", "MU", "QCOM", "NXPI", "AMD", "LRCX",
            "NKE", "UPST", "SOFI", 
            "ABNB", "ORCL", "CRWD", "PLTR", "SNOW", "ZM", "DOCU", "FSLY", "NET",
-           "ZETA", "UBER", "CRDO", "FLNC", "VST", "CLS"
+           "ZETA", "UBER", "CRDO", "FLNC", "VST", "CLS",
+           "MSTR", "BITU"
            ]
 
+print(f"#####START###############################################################")
 # 현재 주가가 20일, 50일, 120일 이동 평균선을 뚫고 상승하는 주식을 찾기
 for ticker in tickers:
     try:
@@ -40,12 +42,28 @@ for ticker in tickers:
         current_volume = data['Volume'].iloc[-1]
         avg_volume_20 = data['Volume'].rolling(window=20).mean().iloc[-1]
 
-
         # 현재 주가가 모든 이동 평균선 위에 있고 거래량이 증가했는지 확인
         if (current_price > data['20_MA'].iloc[-1] and 
             current_price > data['50_MA'].iloc[-1] and 
             current_price > data['120_MA'].iloc[-1] and 
+            data['20_MA'].iloc[-1] > data['50_MA'].iloc[-1] and
+            data['50_MA'].iloc[-1] > data['120_MA'].iloc[-1] and
             current_volume > avg_volume_20):
             print(f"{ticker}: 현재 주가 {current_price} USD가 20일, 50일, 120일 이동 평균선 위에 있으며 거래량이 증가했습니다.")
+            
+           
+        # 현재 주가가 모든 이동 평균선 아래에 있고 거래량이 증가했는지 확인
+        if (current_price < data['20_MA'].iloc[-1] and 
+            current_price < data['50_MA'].iloc[-1] and 
+            current_price < data['120_MA'].iloc[-1] and 
+            data['20_MA'].iloc[-1] < data['50_MA'].iloc[-1] and
+            data['50_MA'].iloc[-1] < data['120_MA'].iloc[-1] and
+            current_volume > avg_volume_20):
+            print(f"{ticker}: 현재 주가 {current_price} USD가 20일, 50일, 120일 이동 평균선 아래에 있으며 거래량이 증가했습니다.")
+
+
+
     except Exception as e:
         print(f"{ticker}: 데이터를 가져오는 중 오류 발생 - {e}")
+
+print(f"#####END#################################################################") 
